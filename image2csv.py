ccf3d9f2cee8dd3a3783fc886d4fd8542cfe7b49
Arthur Perrin <arthur.perrin1298@gmail.com>
2021-02-04 18:41:20 +0100

    form enhancement

--- image2csv.py
+++ image2csv.py
@@ -6,84 +6,105 @@ from tools import pre_process
 from tools import to_matrix
 import numpy as np
 import pytesseract
+import logging
+
+logging.getLogger().setLevel(logging.INFO)
 
 # internal parameters
 window_maximizer = 6
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, type=str, 
-	help="path to input image")
-ap.add_argument("-p", "--path", default='./', type=str, 
-	help="path to output .csv file")
-ap.add_argument("-v", "--visualization", default='n', type=str, help="y/[n] to see each extracted regions individualy")
-ap.add_argument("-m", "--method", type=str, default='fast', 
-	help="[fast]/denoize method to pre-process regions of the input image")
+ap.add_argument("-i", "--image", required=True, type=str, help="path to input image")
+ap.add_argument("-p", "--path", default="./", type=str, help="path to output .csv file")
+ap.add_argument(
+    "-v",
+    "--visualization",
+    default="n",
+    type=str,
+    help="y/[n] to see each extracted regions individualy",
+)
+ap.add_argument(
+    "-m",
+    "--method",
+    type=str,
+    default="fast",
+    help="[fast]/denoize method to pre-process regions of the input image",
+)
 args = vars(ap.parse_args())
 
 
 visu = args["visualization"]
 
+logging.info("Beginning of the script...")
+
 # load the input image from disk
-print('[INFO] Load grayscale image...')
+logging.info("Loading of grayscale image...")
 image = cv2.imread(args["image"])
 
 # create regions to be scanned
-print('[INFO] Extracting regions...')
+logging.info("Extracting regions...")
 boxes = [draw_region(image)]
-boxes,lineLenghts = region_creator(image,boxes)
+boxes, lineLenghts = region_creator(image, boxes)
 
 ROI = []
 for i in range(len(boxes)):
-	X1 = boxes[i][0][0]
-	X2 = boxes[i][1][0]
-	Y1 = boxes[i][0][1]
-	Y2 = boxes[i][1][1]
-	roi = image[Y1:Y2,X1:X2]
-	roi = cv2.cvtColor(roi,cv2.COLOR_BGR2GRAY)
-	(h, w) = roi.shape[:2]
-	roi = cv2.resize(roi, (w*window_maximizer, h*window_maximizer),interpolation=cv2.INTER_CUBIC)
-	ROI.append(roi)
+    X1 = boxes[i][0][0]
+    X2 = boxes[i][1][0]
+    Y1 = boxes[i][0][1]
+    Y2 = boxes[i][1][1]
+    roi = image[Y1:Y2, X1:X2]
+    roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    (h, w) = roi.shape[:2]
+    roi = cv2.resize(
+        roi, (w * window_maximizer, h * window_maximizer), interpolation=cv2.INTER_CUBIC
+    )
+    ROI.append(roi)
 
 # Preprocessing the images
-print("[INFO] Pre-processing the regions with method : "+args["method"]+'...')
-ROI = pre_process(ROI,visu,args["method"])
+logging.info(f"""Pre-processing the regions with method {args["method"]}...""")
+ROI = pre_process(ROI, visu, args["method"])
 
 # Give regions to machine learning model in order to be classified
-print("[INFO] Applying OCR to the regions...")
-custom_config = r'--oem 0 --psm 6'
+logging.info(f"Applying OCR to the regions...")
+custom_config = r"--oem 0 --psm 6"
 numbers = []
 NbError = 0
 for i in range(len(ROI)):
-	region = ROI[i]
-	number = pytesseract.image_to_string(region, config=custom_config)
+    region = ROI[i]
+    number = pytesseract.image_to_string(region, config=custom_config)
 
-	isInt = True
-	try:
-		number = int(number)
-	except ValueError:
-		isInt = False
+    isInt = True
+    try:
+        number = int(number)
+    except ValueError:
+        isInt = False
 
-	if not isInt:
-		number = float("inf")
-		NbError += 1
-		numbers.append(number)
-	else:
-		numbers.append(number)
-	
-	print('OCR applied to '+str(i+1)+' regions out of '+str(len(ROI)))
-	if visu=='y':
-		print('-----------------------------')
-		cv2.imshow('region proceeded (press 0 to close)',region)
-		print("res = ",number)
-		cv2.waitKey(0)
-		cv2.destroyAllWindows()
+    if not isInt:
+        number = float("inf")
+        NbError += 1
+        numbers.append(number)
+    else:
+        numbers.append(number)
 
-print("[INFO] End of OCR, found "+str(NbError)+" errors out of "+str(len(ROI))+" regions...")
-print("[INFO] Exporting to path : "+args["path"]+'... ')
+    logging.debug(f"OCR applied to {str(i + 1)} regions out of {str(len(ROI))}")
 
-out = to_matrix(numbers,lineLenghts)
-print(out)
+    if visu == "y":
+        print("-----------------------------")
+        cv2.imshow("region proceeded (press 0 to close)", region)
+        print("res = ", number)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
-np.savetxt(args["path"]+'output.csv', out, delimiter = ',', fmt='%10.3f')
-print("[INFO] Ending of export (and of the script).")
+logging.info(
+    f"End of OCR, found{str(NbError)} errors out of {str(len(ROI))} regions..."
+)
+
+# Exporting the results
+logging.info(f"""Exporting to path {args["path"]}...""")
+
+out = to_matrix(numbers, lineLenghts)
+
+np.savetxt(args["path"] + "output.csv", out, delimiter=",", fmt="%10.3f")
+
+logging.info("Ending of export (and of the script).")
--- tools.py
+++ tools.py
@@ -1,21 +1,28 @@
 import cv2
 import numpy as np
 
-def imclearborder(imgBW, radius):
-    """ Clear all object that touches the border of one image
-    INPUTS
-        imgBW : source image
-        radius : size of the border
-    OUTPUT
-        imgBWcopy : image in which objects that touched the border have been removed
 
-    This was implemented by Nitish9711 for his Automatic-Number-plate-detection (https://github.com/Nitish9711/Automatic-Number-plate-detection.git)
-    """ 
+def imclearborder(imgBW, radius):
+    """Clear all object that touches at least one border of an image
+
+    Parameters
+    ----------
+    imgBW : numpy.ndarray
+        source image
+    radius : int
+        size of the border
+
+    Returns
+    -------
+    numpy.ndarray
+        image without the objects touching at least one border of the input image
+    """
     imgBWcopy = imgBW.copy()
-    contours,hierarchy = cv2.findContours(imgBWcopy.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    imgRows = imgBW.shape[0]
-    imgCols = imgBW.shape[1]    
-    contourList = [] 
+    contours, hierarchy = cv2.findContours(
+        imgBWcopy.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE
+    )
+    (imgRows, imgCols) = imgBW.shape
+    contourList = []
 
     for idx in np.arange(len(contours)):
         cnt = contours[idx]
@@ -24,111 +31,138 @@ def imclearborder(imgBW, radius):
             rowCnt = pt[0][1]
             colCnt = pt[0][0]
 
-            check1 = (rowCnt >= 0 and rowCnt < radius) or (rowCnt >= imgRows-1-radius and rowCnt < imgRows)
-            check2 = (colCnt >= 0 and colCnt < radius) or (colCnt >= imgCols-1-radius and colCnt < imgCols)
+            check1 = (rowCnt >= 0 and rowCnt < radius) or (
+                rowCnt >= imgRows - 1 - radius and rowCnt < imgRows
+            )
+            check2 = (colCnt >= 0 and colCnt < radius) or (
+                colCnt >= imgCols - 1 - radius and colCnt < imgCols
+            )
 
             if check1 or check2:
                 contourList.append(idx)
                 break
 
     for idx in contourList:
-        cv2.drawContours(imgBWcopy, contours, idx, (0,0,0), -1)
+        cv2.drawContours(imgBWcopy, contours, idx, (0, 0, 0), -1)
 
     return imgBWcopy
 
-def pre_process(images,visu,method):
-    """ Pre-process the images for Tesseract to do a better job
-    INPUTS
-        images : array of images to be pre-processed
-        visu : string to tell if the user wants visualization of the pre-processed image (comparison before/after)
-        method : string to tell what method the user wants (with [slower] or without [faster] denoize)
-    OUTPUT
-        images : array of pre-processed images
+
+def pre_process(images, visu, method):
+    """Pre-process the images for Tesseract to do a better job
+
+    Parameters
+    ----------
+    images : array
+        images to be pre-processed
+    visu : string
+        tell if the user wants visualization of the pre-processed image (comparison before/after)
+    method : string
+        tell what method the user wants (with or without denoize)
+
+    Returns
+    -------
+    array
+        pre-processed images
     """
     for i in range(len(images)):
         imgA = images[i]
 
-        if method=='denoize':
-            imgB = cv2.fastNlMeansDenoising(imgA,None,20)
-            temp,imgB = cv2.threshold(imgB,190,255,cv2.THRESH_BINARY_INV)
+        if method == "denoize":
+            imgB = cv2.fastNlMeansDenoising(imgA, None, 20)
+            temp, imgB = cv2.threshold(imgB, 190, 255, cv2.THRESH_BINARY_INV)
         else:
-            temp,imgB = cv2.threshold(imgA,190,255,cv2.THRESH_BINARY_INV)
+            temp, imgB = cv2.threshold(imgA, 190, 255, cv2.THRESH_BINARY_INV)
 
-        imgB = cv2.morphologyEx(imgB, cv2.MORPH_CLOSE, np.ones((2,2)))
-        imgB = cv2.erode(imgB, np.ones((4,4)))
-        imgB = imclearborder(imgB,1)
+        imgB = cv2.morphologyEx(imgB, cv2.MORPH_CLOSE, np.ones((2, 2)))
+        imgB = cv2.erode(imgB, np.ones((4, 4)))
+        imgB = imclearborder(imgB, 1)
 
-        if visu=='y':
-            cv2.imshow('before segmentation',imgA)        
-            cv2.imshow('after segmentation (press 0 to close)',imgB)
+        if visu == "y":
+            cv2.imshow("before segmentation", imgA)
+            cv2.imshow("after segmentation (press 0 to close)", imgB)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
 
         images[i] = imgB
-        print('Pre-processed '+str(i+1)+' images out of '+str(len(images)))
+        print("Pre-processed " + str(i + 1) + " images out of " + str(len(images)))
     return images
 
-def onmouse(event,x,y,flags,params):
-    """ Function for the user to drawn a rectangle
-    INPUT
-        params : image the user wants to draw a rectangle on
-    OUTPUT
-        rect : coordinates of the rectangle the user has draw
-    """
+
+def onmouse(event, x, y, flags, params):
+    """Function for the user to drawn a rectangle"""
     img = params
     rectangle = None
-    global rect,ix,iy,rect_over
+    global rect, ix, iy, rect_over
     # Draw Rectangle
     if event == cv2.EVENT_LBUTTONDOWN:
         rectangle = True
-        ix,iy = x,y
+        ix, iy = x, y
 
     elif event == cv2.EVENT_MOUSEMOVE:
         if rectangle == True:
-            rect = [(ix,iy),(x,y)]
+            rect = [(ix, iy), (x, y)]
 
     elif event == cv2.EVENT_LBUTTONUP:
         rectangle = False
         rect_over = True
         sceneCopy = img.copy()
-        rect = [(ix,iy),(x,y)]
-        cv2.rectangle(sceneCopy,rect[0],rect[1],(0,0,255))
-        cv2.imshow('Drawn rectangle (press 0 to close)',sceneCopy)
+        rect = [(ix, iy), (x, y)]
+        cv2.rectangle(sceneCopy, rect[0], rect[1], (0, 0, 255))
+        cv2.imshow("Drawn rectangle (press 0 to close)", sceneCopy)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
     else:
         rect = None
 
+
 def draw_region(img):
-    """ Driver function for the user to draw a rectangle
-    INPUT
-        img : image the user wants to draw a rectangle on
-    OUTPUT
-        rect : coordinates of the rectangle the user has draw
+    """Driver function for the user to draw a rectangle
+
+    Parameters
+    ----------
+    img : numpy.dnarray
+        image to draw a rectangle on
+
+    Returns
+    -------
+    array
+        coordinates of the rectangle the user has draw
     """
     while True:
         # displaying the image
-        cv2.imshow('image (draw a rectangle)', img) 
-    
-        # setting mouse hadler for the image 
-        # and calling the click_event() function 
-        cv2.setMouseCallback('image (draw a rectangle)', onmouse, img) 
-    
-        # wait for a key to be pressed to exit 
+        cv2.imshow("image (draw a rectangle)", img)
+
+        # setting mouse hadler for the image
+        # and calling the click_event() function
+        cv2.setMouseCallback("image (draw a rectangle)", onmouse, img)
+
+        # wait for a key to be pressed to exit
         cv2.waitKey(0)
         # close the window
         cv2.destroyAllWindows()
         return rect
 
+
 def region_creator(img, initBoxes):
-    """ Create a grid on an image given an initial region
-    INPUTS 
-        img : image the user wants a grid to be drawn on
-        initBoxes : the initial rectangle coordinates to build the grid from
-    OUTPUTS
-        endBoxes : coordinates of each regions composing the grid
-        lineLenghts : number of regions of each grid's line
+    """Create a grid on an image given an initial region
+
+    Parameters
+    ----------
+    img : numpy.ndarray
+        image the user wants a grid to be drawn on
+    initBoxes : array
+        the initial rectangle coordinates to build the grid from
+
+    Returns
+    -------
+    endBoxes : array
+        coordinates of each regions composing the grid
+
+    lineLenghts : array
+        number of regions in each grid's line
     """
+
     xi = initBoxes[0][0][0]
     yi = initBoxes[0][0][1]
     wi = initBoxes[0][1][0]
@@ -141,72 +175,87 @@ def region_creator(img, initBoxes):
     initPos = initBoxes[0][0][0]
 
     lineLenghts = []
-    endBoxes = [[(xi,yi),(wi,hi)]]
+    endBoxes = [[(xi, yi), (wi, hi)]]
 
     Xoffset = 0
     Yoffset = 0
     search = True
     test = 0
     while search:
-        print('----------------------------------')
-        print('Automatic region extractor : enter offsets values for the grid to cover all numbers (end with 0 0)')
-        Xoffset = float(input('X offset ? : (previously = '+str(Xoffset)+')  '))
-        Yoffset = float(input('Y offset ? : (previously = '+str(Yoffset)+')  '))
-        if int(Xoffset)==0 and int(Yoffset)==0 and test!=0:
+        print("----------------------------------")
+        print(
+            "Automatic region extractor : enter offsets values for the grid to cover all numbers (end with 0 0)"
+        )
+        Xoffset = float(input(f"X offset ? : (previously = {str(Xoffset)}) "))
+        Yoffset = float(input(f"Y offset ? : (previously = {str(Yoffset)}) "))
+        if int(Xoffset) == 0 and int(Yoffset) == 0 and test != 0:
             search = False
             cv2.destroyAllWindows()
         else:
             cv2.destroyAllWindows()
-            test+=1
-            pos = [initPos,initBoxes[0][0][1]]
-            endBoxes = [[(xi,yi),(wi,hi)]]
+            test += 1
+            pos = [initPos, initBoxes[0][0][1]]
+            endBoxes = [[(xi, yi), (wi, hi)]]
             lineLenghts = []
             raw = 0
             while True:
-                width_test = pos[0]+0.5*(Xshift+Xoffset)>=width
-                height_test = pos[1]+0.5*(Yshift+Yoffset)>=height
-                if width_test or height_test: # if their are no more regions on the right
-                    if height_test: # if their are no more regions below
+                width_test = pos[0] + 0.5 * (Xshift + Xoffset) >= width
+                height_test = pos[1] + 0.5 * (Yshift + Yoffset) >= height
+                if (
+                    width_test or height_test
+                ):  # if there are no more regions on the right
+                    if height_test:  # if there are no more regions below
                         break
-                    else: # restart from the line below
-                        pos[1] += (Yshift+int(Yoffset))
+                    else:  # restart from the line below
+                        pos[1] += Yshift + int(Yoffset)
                         pos[0] = initPos
                         lineLenghts.append(raw)
                         raw = 0
-                else: # if their are still regions on the right
-                    newBox = [(pos[0],pos[1]),(pos[0]+int(Xshift),pos[1]+int(Yshift))]
-                    pos[0] += (Xshift+int(Xoffset))
+                else:  # if there are still regions on the right
+                    newBox = [
+                        (pos[0], pos[1]),
+                        (pos[0] + int(Xshift), pos[1] + int(Yshift)),
+                    ]
+                    pos[0] += Xshift + int(Xoffset)
                     endBoxes.append(newBox)
-                    raw+=1
+                    raw += 1
 
-        print('Number of regions extracted :', len(endBoxes))
+        print(f"Number of regions extracted : {len(endBoxes)}")
 
         # Show the regions over the image
         if search:
             imgCopy = img.copy()
             for i in range(len(endBoxes)):
-                (x,y) = endBoxes[i][0]
-                (w,h) = endBoxes[i][1]
-                cv2.rectangle(imgCopy, (x,y), (w,h), (0,0,255), 1)
-            cv2.imshow('(temporary) extracted regions (press 0 to close)',imgCopy)
+                (x, y) = endBoxes[i][0]
+                (w, h) = endBoxes[i][1]
+                cv2.rectangle(imgCopy, (x, y), (w, h), (0, 0, 255), 1)
+            cv2.imshow("(temporary) extracted regions (press 0 to close)", imgCopy)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
     endBoxes.pop(0)
     return endBoxes, lineLenghts
 
-def to_matrix(el,lineLenghts):
-    """ Convert a list to a matrix given the number of elements per line
-    INPUTS
-        el : list to be converted
-        lineLenghts : list of number of elements per line to build the matrix
-    OUTPUT
-        res : array of arrays representing the desired matrix (numpy formated)
+
+def to_matrix(el, lineLenghts):
+    """Convert a list to a matrix given the number of elements per line
+
+    Parameters
+    ----------
+    el : array
+        list to be converted
+    lineLenghts : array
+        list of number of elements per line to build the matrix
+
+    Returns
+    -------
+    array
+        lines of the matrix formed by the elements of the input list
     """
     res = []
     for i in range(len(lineLenghts)):
         lineLenght = lineLenghts[i]
         temp = []
         for j in range(lineLenght):
-            temp.append(el[j+i*lineLenght])
+            temp.append(el[j + i * lineLenght])
         res.append(temp)
     return np.array(res)
